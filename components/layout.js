329d93fdfec4044566b95fa48e8139d16842fe52
JulioSesai <antonio.sesai@gmail.com>
2023-09-25 22:04:37 -0600

    Iniciando proyecto

---
+++ components/layout.js
@@ -0,0 +1,11 @@
+
+
+export default function Layout({children}) {
+  return (
+    <>
+        <h1>Desde layout</h1>
+      {children}
+    </>
+  )
+}
+
---
+++ pages/nosotros.js
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import Layout from "@/components/layout"
+function Nosotros() {
+  return (
+    <div>
+        <Layout>
+
+            <h1>Desde nosotros.js</h1>
+            <Link href="./">Ir a inicio</Link>
+        </Layout>
+    </div>
+  )
+}
+
+export default Nosotros
